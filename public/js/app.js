5b223007bd4ebb7edb059d9a8210e9371d266445
Brenner Haverlock <80720533+officialbrenner@users.noreply.github.com>
2021-10-08 16:18:23 -0700

    complete + added doge support

--- public/js/app.js
+++ public/js/app.js
@@ -14,6 +14,10 @@ socket.on('eth-prices', tickerdata => {
     refreshETHTicker(tickerdata);
 });
 
+socket.on('doge-prices', tickerdata => {
+    refreshDOGETicker(tickerdata);
+});
+
 
 function refreshBTCTicker(tickerdata) {
     const tblBTC = document.getElementById("btc-ticker");
@@ -84,6 +88,43 @@ function refreshETHTicker(tickerdata) {
 	});
 
 }
+
+
+function refreshDOGETicker(tickerdata) {
+    const tbldoge = document.getElementById("doge-ticker");
+    
+
+	const tblRow = tbldoge.getElementsByTagName("tr");
+	let rowCnt = 0;
+
+	tickerdata.map((item) => {
+		for (let i = 0; i < tblRow.length; i++) {
+			const tblCol = tblRow[i].getElementsByTagName("td")[0];
+      if (tblCol) {
+				const txtValue = tblCol.getAttribute('data-currency');
+        if (txtValue === '' || txtValue === item.target) {
+          tbldoge.deleteRow(i);
+        }
+      }   
+		}
+
+		const tbldogebody = document.getElementById('doge-ticker').getElementsByTagName('tbody')[0];
+		const newRow = tbldogebody.insertRow(rowCnt);
+		const cell1 = newRow.insertCell(0);
+		const cell2 = newRow.insertCell(1);
+		const cell3 = newRow.insertCell(2);
+		const cell4 = newRow.insertCell(3);
+		const cell5 = newRow.insertCell(4);
+		cell1.innerHTML = printCurrency(item.target);
+		cell1.setAttribute('data-currency', item.target);
+		cell2.innerHTML = item.price;
+		cell3.innerHTML = item.volume;
+		cell4.innerHTML = item.change;
+		rowCnt++;
+	});
+
+}
+
 
 function formatDate() {
 	const date = new Date();
